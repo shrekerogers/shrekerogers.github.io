063869d77e95a82606c092ff38dc7f39bb3ace92
Sarita Rogers <shrekerogers@byui.edu>
2020-02-01 10:53:53 -0700

    1.2

--- week-four/js/script.js
+++ week-four/js/script.js
@@ -1,5 +1,4 @@
-/*!--javascript*/
-
-/*This will be the page that has the javascript for the footer
-to present the date.  format Wednesday, 24 July 2020*/
-
+function toggleMenu()   {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
