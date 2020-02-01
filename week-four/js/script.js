76f8748096b25b65308ac8eca9614c4e15e5e87d
Sarita Rogers <shrekerogers@byui.edu>
2020-02-01 13:03:08 -0700

    1.5

--- week-four/js/script.js
+++ week-four/js/script.js
@@ -1,4 +1,5 @@
 function toggleMenu()   {
-    
-    document.getElementById("primaryNav").classList.toggle("hide");
+       
+        document.getElementById("primaryNav").classList.toggle("hide")
+        
 }
