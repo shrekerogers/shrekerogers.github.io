09cf823ede90eb660615dda7f61196987e56762b
Sarita Rogers <shrekerogers@byui.edu>
2020-03-03 12:27:24 -0700

    ii8i

---
+++ week-nine/prophets.js
@@ -0,0 +1,13 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  const prophets = jsonObject['prophets'];
+  });
+
+
+
