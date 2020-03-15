3e31db0747d189426a884e68186960faa6b32bf4
Sarita Rogers <shrekerogers@byui.edu>
2020-03-14 20:07:02 -0600

    cbxcbcxdbhxc

--- week-ten/java/weatherapi.js
+++ week-ten/java/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weatherid=5604473&units=imperial&APPID= {ee1ecbe33adbc9091418e495f826c8ae}";
+const apiURL = 'http://api.openweathermap.org/data/2.5/weatherid=5604473&units=imperial&APPID={ee1ecbe33adbc9091418e495f826c8ae}';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
