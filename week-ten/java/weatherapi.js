db1f13c103c38e571e20b95c9b88d5eb8a6d35d8
Sarita Rogers <shrekerogers@byui.edu>
2020-03-16 11:14:57 -0600

    16.1

--- week-ten/java/weatherapi.js
+++ week-ten/java/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weatherid=5604473&units=imperial&APPID={ee1ecbe33adbc9091418e495f826c8ae}";
+const apiURL = "http://api.openweathermap.org/date/2.5/weather?id=560447&units=imperial&APPID=ee1ecbe33adbc9091418e495f826c8ae";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -8,7 +8,7 @@ fetch(apiURL)
     currentTemp.textContent = jsObject.main.temp;
 
 
-    const imagesrc = `https://openweathermap.org/img/w/' ${jsonObject.weather[0].icon}.png`;  // note the concatenation
+    const imagesrc = `' ${jsonObject.weather[0].icon}.png`;  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
