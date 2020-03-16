43cf65f1ab13c18c7a580b256aea311ace9a520f
Sarita Rogers <shrekerogers@byui.edu>
2020-03-16 11:26:51 -0600

    16.4

--- week-ten/java/weatherapi.js
+++ week-ten/java/weatherapi.js
@@ -1,10 +1,11 @@
 const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID={ee1ecbe33adbc9091418e495f826c8ae}';
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     const currentTemp = documemt.querySelector('#current-temp');
-    const weatherIcon = document.querySelector('#imagesrc')
+    const weatherIcon = document.querySelector('#imagesrc');
     currentTemp.textContent = jsObject.main.temp;
 
 
