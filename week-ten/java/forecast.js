bafbd2ff671583058ae89d051d3366d69e61e54e
Sarita Rogers <shrekerogers@byui.edu>
2020-03-16 16:14:25 -0600

    ;k;k;jk;jk;

--- week-ten/java/forecast.js
+++ week-ten/java/forecast.js
@@ -1,11 +1,14 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID={ee1ecbe33adbc9091418e495f826c8ae}';
+const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=ee1ecbe33adbc9091418e495f826c8ae';
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     
-   
+   const fivedayforecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
+    console.log(fivedayforecast);
+
+    for (let i = 0; i<fivedayforecast.length; i++);
   });
 
 
