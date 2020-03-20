0ac6f17ddba9452d0b56ca3627f9a11c829bab28
Sarita Rogers <shrekerogers@byui.edu>
2020-03-19 18:56:52 -0600

    19.8

---
+++ week-ten/java/main-weatherapi.js
@@ -0,0 +1,39 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=ee1ecbe33adbc9091418e495f826c8ae';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    
+    console.log(jsObject);
+    
+    const currentTemp = document.querySelector('#current-temp');
+    const weatherIcon = document.querySelector('#imagesrc');
+
+
+    currentTemp.textContent = jsObject.main.temp;
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`; 
+    
+const desc = jsObject.weather[0].description; 
+
+document.getElementById('imagesrc').textContent = imagesrc;  
+
+document.getElementById('icon').setAttribute('src', imagesrc);  
+
+document.getElementById('icon').setAttribute('alt', desc);
+
+
+  
+
+ 
+  });
+
+
+
+
+
+
+
+
+
+  
