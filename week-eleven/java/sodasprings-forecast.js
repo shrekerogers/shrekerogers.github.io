91af10e67473deec6b744b2053c47171d8f1069d
Sarita Rogers <shrekerogers@byui.edu>
2020-03-21 14:21:54 -0600

    21.6

---
+++ week-eleven/java/sodasprings-forecast.js
@@ -0,0 +1,22 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=ee1ecbe33adbc9091418e495f826c8ae';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    
+    console.log(jsObject);
+
+
+ const fivedayforecast = jsObject.list.filter(x => 
+ x.dt_txt.includes('18:00:00'));
+
+for (let i = 0; i <fivedayforecast.length; i++) {
+    document.getElementById(`forecast${i=1}`).
+    textContent = fivedayforecast[i].min.temp;
+
+    
+}
+
+
+
+});
